2a49f2b32b075b7936cb32a82586553127e2c874
Cauã de Lima <caua-l1@outlook.com>
2026-07-02 21:26:46 -0300

    chore: informações iniciais da aplicação

---
+++ src/main/java/com/github/caualima17/intelli_code_ldi_agent/IntelliCodeLdiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.github.caualima17.intelli_code_ldi_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliCodeLdiAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelliCodeLdiAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/caualima17/intelli_code_ldi_agent/IntelliCodeLdiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.caualima17.intelli_code_ldi_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelliCodeLdiAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
